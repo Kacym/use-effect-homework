b274ef6a7f33a45f5f5a35e0fbcf226ffa63d895
Kasym <ulanbek.kasym@gmail.com>
2024-01-28 20:13:23 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const [consoleInputValue, setConsoleInputValue] = useState("");
+  const [localStorageInputValue, setLocalStorageInputValue] = useState("");
+
+  const getConsoleInputValue = (e) => {
+    setConsoleInputValue(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(consoleInputValue);
+  }, [consoleInputValue])
+
+  // local storage input
+
+  const getLocalStorageInputValue = (e) => {
+    setLocalStorageInputValue(e.target.value)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('local', localStorageInputValue)
+  }, [localStorageInputValue])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+      <button onClick={() => setUsers([])}>удалить пользователей</button>
+      <button onClick={getUsers}>получить пользователей</button>
+
+      <hr />
+
+      <input onChange={getConsoleInputValue} placeholder="консоль" />
+      <input onChange={getLocalStorageInputValue} placeholder="local storage" />
     </div>
   );
 }
